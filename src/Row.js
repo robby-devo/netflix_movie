85cc385eab4cc3d83d53f2efc603bee5b3151cbc
Robby Dongre <86415240+robby-devo@users.noreply.github.com>
2022-03-27 17:37:27 +0530

    This is netflix movie application

---
+++ src/Row.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+import "./Row.css";
+
+function Row({ fetchUrl, title, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const base_url = "https://image.tmdb.org/t/p/original";
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await axios.get(fetchUrl);
+      setMovies(res.data.results);
+      return res;
+    }
+    fetchMovies();
+  }, [fetchUrl]);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.original_title || movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={movie.id}
+                onClick={() => handleClick(movie)}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+export default Row;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBaV1uY04d9xwVLYyc8qEpNfrqEN_d7QeA",
+  authDomain: "netlix-clone-ytube.firebaseapp.com",
+  projectId: "netlix-clone-ytube",
+  storageBucket: "netlix-clone-ytube.appspot.com",
+  messagingSenderId: "618982672258",
+  appId: "1:618982672258:web:d25bf5d0f4546fffcbf19b",
+};
